f949a00f998ea34670dfdd0e862dc15e5de1fb8b
Wilson Bundi <darliassoftware@gmail.com>
2026-05-11 06:43:54 +0300

    Restore BrowserRouter and add Azure Static Web App SPA fallback config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter
+    <BrowserRouter
       future={{
         v7_startTransition: true,
         v7_relativeSplatPath: true,
       }}
     >
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
